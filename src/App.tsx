c4a58f2d404ad9ba8e30b317beddcdcbe5956892
boomtnt2843 <boomphat@odds.team>
2024-10-24 23:05:00 +0700

    :hammer: add fetch api & main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Todo } from "./type";
+import TodoList from "./TodoList";
+import TodoForm from "./Form";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const getTodosData = async () => {
+    try {
+      const config: RequestInit = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:3001/todos", config);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setTodos(data);
+    } catch (err) {
+      console.log("error::: ", err);
+    }
+  };
+
+  const createTask = async (task: string) => {
+    const body = {
+      text: task
+    }
+    const config: RequestInit = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    };
+    await fetch("http://localhost:3001/todos", config);
+  };
+
+  const onSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      task: {value: string}
+    }
+    
+    await createTask(formElements.task.value);
+    
+    form.reset(); 
+
+    await getTodosData();
+  };
+
+  useEffect(() => {
+    getTodosData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODOS</h1>
+      <TodoForm onSubmit={onSubmit} />
+      <TodoList todos={todos} />
     </div>
   );
 }
